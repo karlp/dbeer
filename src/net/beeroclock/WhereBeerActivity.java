cdb86cbe94d4a53030d771ceae16e70772579f8b
Karl Palsson <github@tweak.net.au>
2011-03-09 02:35:36 +0000

    Very rough demo of displaying location updates on a tab panel.

--- src/net/beeroclock/WhereBeerActivity.java
+++ src/net/beeroclock/WhereBeerActivity.java
@@ -1,18 +1,57 @@
 package net.beeroclock;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
-public class WhereBeerActivity extends Activity
-{
-    /** Called when the activity is first created. */
+import java.util.List;
+
+public class WhereBeerActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        TextView tv = new TextView(this);
 //        tv.setText("Hello Android");
         setContentView(R.layout.wherebeer);
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        List l = locationManager.getAllProviders();
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+//        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        // emulator has gps, but no network?!
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+    }
+
+    private void makeUseOfNewLocation(Location location) {
+        TextView tv = (TextView) findViewById(R.id.tv_wherebeer);
+        tv.append(location.toString());
     }
 }
