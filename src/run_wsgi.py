6bf033e4a29bd70a05f5aaeac870785b8c270d01
Karl Palsson <github@tweak.net.au>
2011-03-16 12:05:22 +0000

    Return price and distance information as well, not just the bar names.

--- src/run_wsgi.py
+++ src/run_wsgi.py
@@ -6,6 +6,7 @@ __date__ ="$Mar 15, 2011 7:24:21 PM$"
 
 import json
 import logging
+import random
 
 import bottle
 from bottle import route, run, request, response, abort
@@ -17,9 +18,7 @@ config = {
 }
 
 
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(name)s - %(message)s",
-#filename="/var/log/karlnet_pachube.log"
-)
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(levelname)s %(name)s - %(message)s")
 log = logging.getLogger("main")
 
 
@@ -41,9 +40,15 @@ def bars_nearest(num=3):
         log.debug("Squashing request for %d bars down to %d", num, config['results_limit'])
         num = config['results_limit']
 
-    # TODO - we actually want to return the distance here too, not jus the bars
     nearest = sorted(od.bars, key=lambda b: b.distance((lon, lat)))
-    return json.dumps(nearest[:num], default=models.Bar.to_json)
+    results = []
+    # TODO - we should return a nicer datatype here, with "links" to more info...
+    # look at the rest media types docs you have
+    for i,v in enumerate(nearest[:num]):
+        results.append({"bar" : v,
+                "distance" : v.distance((lon,lat)),
+                "prices" : { "beer" : random.randrange(500, 950, 50)}})
+    return json.dumps(results, default=models.Bar.to_json)
 
 @route('/hello')
 def wopwop():
