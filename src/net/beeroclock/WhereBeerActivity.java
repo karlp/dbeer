728242a56171a49d41e1ac15103718c14859cea8
Karl Palsson <github@tweak.net.au>
2011-03-18 12:46:34 +0000

    Use a custome ArrayAdaptor to populate our list of nearby bars with the WS results.
    UI layout is still far from perfect

--- src/net/beeroclock/WhereBeerActivity.java
+++ src/net/beeroclock/WhereBeerActivity.java
@@ -1,39 +1,32 @@
 package net.beeroclock;
 
-import android.app.Activity;
+import android.app.ListActivity;
 import android.content.Context;
-import android.content.res.Resources;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import java.io.*;
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.List;
-import java.util.NavigableSet;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
-public class WhereBeerActivity extends Activity {
+public class WhereBeerActivity extends ListActivity {
     /**
      * Called when the activity is first created.
      */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        TextView tv = new TextView(this);
-//        tv.setText("Hello Android");
         setContentView(R.layout.wherebeer);
 
         // Acquire a reference to the system Location Manager
@@ -64,28 +57,61 @@ public class WhereBeerActivity extends Activity {
     }
 
     private void makeUseOfNewLocation(Location location) {
-        TextView tv = (TextView) findViewById(R.id.tv_wherebeer);
-//        tv.append(location.toString());
-        URL u;
         String xmlr;
         //String host = "tera.beeroclock.net";
         String host = "192.168.149.34:5000";
 
-        String uu = "http://" + host + "/nearest.xml/5?lat=" + location.getLatitude() + "&lon=" + location.getLongitude();
+        String uu = "http://" + host + "/nearest.xml/10?lat=" + location.getLatitude() + "&lon=" + location.getLongitude();
 
         HttpClient client = new DefaultHttpClient();
         HttpGet request = new HttpGet(uu);
-        String content;
         try {
-             xmlr = client.execute(request, new BasicResponseHandler());
+            xmlr = client.execute(request, new BasicResponseHandler());
         } catch (IOException e) {
             throw new IllegalStateException("hatred", e);
         }
 
         Set<Bar> bars = Utils.parseBarXml(xmlr);
-        // great, now what?
-        Bar next = bars.iterator().next();
-        tv.append("\r\nFound bars, closest is " + next.name + ", which is " + next.distance + "m away");
+        BarArrayAdapter arrayAdapter = new BarArrayAdapter(this, R.layout.where_row_item, new ArrayList<Bar>(bars));
+        ListView lv = getListView();
+        lv.setAdapter(arrayAdapter);
     }
 
+    public class BarArrayAdapter extends ArrayAdapter<Bar> {
+        private Context context;
+        private ArrayList<Bar> items;
+
+        public BarArrayAdapter(Context context, int textViewResourceId, ArrayList<Bar> objects) {
+            super(context, textViewResourceId, objects);
+            this.context = context;
+            this.items = objects;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View view = convertView;
+
+            // karl - no idea what this is for yet, comes from http://stackoverflow.com/questions/2265661/how-to-use-arrayadaptermyclass
+            // I believe this is if we somehow are in a state where the view is being requested, but has been gc'd, perhaps returning to this activity?
+            if (view == null) {
+                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                view = inflater.inflate(R.layout.where_row_item, null);
+            }
+
+            Bar bar = items.get(position);
+
+            if (bar != null) {
+                TextView distanceView = (TextView) view.findViewById(R.id.bar_distance);
+                if (distanceView != null) {
+                    distanceView.setText(String.format("%4.1fm", bar.distance));
+                }
+                TextView nameView = (TextView) view.findViewById(R.id.bar_name);
+                if (nameView != null) {
+                    nameView.setText(bar.name);
+                }
+            }
+
+            return view;
+        }
+    }
 }
