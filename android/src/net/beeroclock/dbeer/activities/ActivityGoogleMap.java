61b754eed3b858c059ee52b87b5caf9e5e84d6b8
Karl Palsson <karlp@tweak.net.au>
2011-04-20 18:00:30 +0000

    Show our current location on the google map view as well, closes #20 at codaset.
    Could be better, we should be able to draw _our_ location immediately, as we already know it.  Also, as you move, google recenters the map on your current location :(

--- android/src/net/beeroclock/dbeer/activities/ActivityGoogleMap.java
+++ android/src/net/beeroclock/dbeer/activities/ActivityGoogleMap.java
@@ -2,7 +2,9 @@ package net.beeroclock.dbeer.activities;
 
 import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.graphics.drawable.Drawable;
+import android.location.Location;
 import android.os.Bundle;
 import com.google.android.maps.*;
 import net.beeroclock.dbeer.models.Bar;
@@ -20,6 +22,11 @@ import java.util.List;
  */
 public class ActivityGoogleMap extends MapActivity {
 
+    private MyLocationOverlay hereOverlay;
+    private PintyApp pinty;
+    private Bar bar;
+    private MapView mapView;
+
     @Override
     protected boolean isRouteDisplayed() {
         // required for google accounting...
@@ -30,7 +37,7 @@ public class ActivityGoogleMap extends MapActivity {
     protected void onCreate(Bundle bundle) {
         super.onCreate(bundle);
         setContentView(R.layout.google_map);
-        MapView mapView = (MapView) findViewById(R.id.google_map_view);
+        mapView = (MapView) findViewById(R.id.google_map_view);
         mapView.setBuiltInZoomControls(true);
 
         Bundle extras = getIntent().getExtras();
@@ -38,17 +45,42 @@ public class ActivityGoogleMap extends MapActivity {
         if (barId == null) {
             throw new IllegalStateException("It should be impossible to get here: 'show on map' with no barid");
         }
-        PintyApp pinty = (PintyApp)getApplication();
-        Bar bar = pinty.getBar(barId);
+        pinty = (PintyApp)getApplication();
+        bar = pinty.getBar(barId);
 
         List<Overlay> existingMapOverlays = mapView.getOverlays();
         // FIXME - again with the whole artwork thing, a bar icon...
         Drawable drawable = this.getResources().getDrawable(R.drawable.ic_menu_goto);
         BarItemizedOverlay barOverlay = new BarItemizedOverlay(drawable, this);
         barOverlay.addBar(bar);
+
+        hereOverlay = new MyLocationOverlay(this, mapView);
+        hereOverlay.enableMyLocation();
+
         existingMapOverlays.add(barOverlay);
-        mapView.getController().animateTo(makeGeoPoint(bar));
-        mapView.getController().setZoom(16);  // this seems reasonable?
+        existingMapOverlays.add(hereOverlay);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        GeoPoint barGeo = makeGeoPoint(bar);
+        Location loc = pinty.getLastLocation();
+        GeoPoint here = new GeoPoint((int)(loc.getLatitude() * 1e6), (int)(loc.getLongitude() * 1e6));
+
+        int maxLat = Math.max(barGeo.getLatitudeE6(), here.getLatitudeE6());
+        int minLat = Math.min(barGeo.getLatitudeE6(), here.getLatitudeE6());
+        int maxLon = Math.max(barGeo.getLongitudeE6(), here.getLongitudeE6());
+        int minLon = Math.min(barGeo.getLongitudeE6(), here.getLongitudeE6());
+        mapView.getController().zoomToSpan(Math.abs(maxLat - minLat), Math.abs(maxLon - minLon));
+        mapView.getController().animateTo(new GeoPoint((maxLat + minLat) / 2, (maxLon + minLon) / 2));
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        hereOverlay.disableMyLocation();
     }
 
     private GeoPoint makeGeoPoint(Bar bar) {
@@ -89,6 +121,11 @@ public class ActivityGoogleMap extends MapActivity {
             AlertDialog.Builder dialog = new AlertDialog.Builder(context);
             dialog.setTitle(item.getTitle());
             dialog.setMessage(item.getSnippet());
+            dialog.setNeutralButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            });
             dialog.show();
             return true;
         }
