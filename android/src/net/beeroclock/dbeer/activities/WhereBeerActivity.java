13a102d37c0ec6d99834fbe5023339a94e90b447
Karl Palsson <karlp@tweak.net.au>
2011-04-29 00:42:27 +0000

    Allow people to cancel the "lost" dialog, and improve the text shown there while we're waiting.
    closes BF-10 in the bug genie

--- android/src/net/beeroclock/dbeer/activities/WhereBeerActivity.java
+++ android/src/net/beeroclock/dbeer/activities/WhereBeerActivity.java
@@ -263,7 +263,7 @@ public class WhereBeerActivity extends ListActivity implements LocationListener,
             // oh well, nothing viable...  FIXME - should update this periodically, letting them know we're still trying...
             // Can we even do that? we're registered for updates, that's about as good as we can do!
             lostDialog = ProgressDialog.show(this, getResources().getString(R.string.dialog_lost_title),
-                    getResources().getString(R.string.dialog_lost_message), true, false);
+                    getResources().getString(R.string.dialog_lost_message), true, true);
         }
     }
 
